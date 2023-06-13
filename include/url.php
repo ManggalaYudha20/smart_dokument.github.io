bb99310aac95d53aa6515a257949e409c062e1df
ManggalaYudha20 <132431405+ManggalaYudha20@users.noreply.github.com>
2023-06-13 12:56:52 +0800

    Update url.php

--- include/url.php
+++ include/url.php
@@ -1,3 +1,4 @@
 <?php
-echo "https://smart-dokument-word.000webhostapp.com/";
+echo "https://smart_dokument.github.io/";
+//echo "https://smart-dokument-word.000webhostapp.com/";
 // echo "https://arsip-baplitbang-bonebolango.com/";
